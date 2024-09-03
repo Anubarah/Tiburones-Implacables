a19b2a20b53800adcaac4900296bf854fb2db075
Anub'arah <Anub'arah@DESKTOP-696UE0B>
2024-09-02 19:14:53 -0500

    Agregar archivos de proyecto.

---
+++ Cristian D/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
